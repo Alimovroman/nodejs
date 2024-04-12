1b0b7528ac445422e7c160d9af951ad7bc3676c0
Roman <Alimov.ramon@gmail.com>
2024-04-12 14:09:12 +0300

    add middleware

--- dist/index.js
+++ dist/index.js
@@ -9,10 +9,38 @@ const product_router_1 = require("./routers/product-router");
 const addresses_router_1 = require("./routers/addresses-router");
 const app = (0, express_1.default)();
 const port = 3000;
+const blablaMiddleware = (req, res, next) => {
+    //@ts-ignore
+    req.blabla = "hello";
+    next();
+};
+const authMiddleware = (req, res, next) => {
+    //@ts-ignore
+    req.blabla = "hello";
+    if (req.query.token === "123") {
+        next();
+    }
+    else {
+        res.send(401);
+    }
+};
+let count = 0;
+const counterMiddleware = (req, res, next) => {
+    count++;
+    next();
+};
 const middleWare = (0, body_parser_1.default)({});
 app.use(middleWare);
+app.use(counterMiddleware);
+app.use(blablaMiddleware);
+app.use(authMiddleware);
 app.use("/products", product_router_1.productsRouter);
 app.use("/addresses", addresses_router_1.addressesRouter);
+app.get("/test", (req, res) => {
+    //@ts-ignore
+    const blabla = req.blabla;
+    res.send({ value: blabla + "!!!" + count });
+});
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import bodyParser from "body-parser";
 import { productsRouter } from "./routers/product-router";
 import { addressesRouter } from "./routers/addresses-router";
@@ -6,11 +6,40 @@ import { addressesRouter } from "./routers/addresses-router";
 const app = express();
 const port = 3000;
 
+const blablaMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  //@ts-ignore
+  req.blabla = "hello";
+  next();
+};
+const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  //@ts-ignore
+  req.blabla = "hello";
+  if (req.query.token === "123") {
+    next();
+  } else {
+    res.send(401);
+  }
+};
+let count = 0;
+const counterMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  count++;
+  next();
+};
+
 const middleWare = bodyParser({});
 app.use(middleWare);
+app.use(counterMiddleware);
+app.use(blablaMiddleware);
+app.use(authMiddleware);
 
 app.use("/products", productsRouter);
 app.use("/addresses", addressesRouter);
+
+app.get("/test", (req: Request, res: Response) => {
+  //@ts-ignore
+  const blabla = req.blabla;
+  res.send({ value: blabla + "!!!" + count });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
